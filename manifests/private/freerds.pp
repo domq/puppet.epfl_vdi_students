05d28223aed35b44c2de004c28b5e3b5c5337407
Dominique Quatravaux <dominique.quatravaux@epfl.ch>
2016-10-06 17:46:42 +0200

    (WIP) Configuration and scripts for new version of FreeRDS

---
+++ manifests/private/freerds.pp
@@ -0,0 +1,23 @@
+class epflsti_vdi_students::private::freerds() {
+   file { [  "/opt", "/opt/FreeRDS", "/opt/FreeRDS/share", "/opt/FreeRDS/sbin",
+             "/opt/FreeRDS/etc", "/opt/FreeRDS/etc/freerds" ]:
+    ensure => "directory"
+  }
+  file { "/opt/FreeRDS/share/start_script_lib.sh":
+    content => template("epflsti_vdi_students/start_script_lib.sh.erb"),
+  }
+
+  file { "/opt/FreeRDS/sbin/start_greeter":
+    content => template("epflsti_vdi_students/start_greeter.erb"),
+    mode => "0755"
+  }
+
+  file { "/opt/FreeRDS/sbin/start_xsession":
+    content => template("epflsti_vdi_students/start_xsession.erb"),
+    mode => "0755"
+  }
+
+  file { "/opt/FreeRDS/etc/freerds/config.ini":
+    content => template("epflsti_vdi_students/config.ini.erb"),
+  }
+}
