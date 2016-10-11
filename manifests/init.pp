68d12e2c8a1117f220df8cbd12fa6927bda0205d
Dominique Quatravaux <dominique.quatravaux@epfl.ch>
2016-10-11 11:51:47 +0200

    (WIP) Sprout epflsti_vdi_students::private::freerds

--- manifests/init.pp
+++ manifests/init.pp
@@ -7,14 +7,6 @@ class epflsti_vdi_students(
   class { "epfl_sso": }
   class { "epfl_sso::krb5": }
 
-  # TODO: this hasn't really been tested and in fact, is quite unlikely
-  # to work. E.g. the script install /usr/local/bin/cmake which links
-  # to a libcurl that doesn't SSL.
-  exec { "wget -O- https://raw.githubusercontent.com/epfl-sti/FreeRDS/master/build-freerds.sh | bash":
-    path => $::path,
-    creates => "/opt/FreeRDS/bin/freerds-manager"
-  }
-
   # Grant a bunch of groups to everyone.
   file { "/etc/security/group.conf":
     content => template("epflsti_vdi_students/group.conf.erb")
@@ -38,6 +30,8 @@ class epflsti_vdi_students(
       fail("${::osfamily} not supported for pam_group configuration")
     }
   }
+
+  class { "epflsti_vdi_students::private::freerds": }
   
   if ($finalize) {
     stage { "finalize":
---
+++ manifests/private/freerds.pp
@@ -0,0 +1,29 @@
+class epflsti_vdi_students::private::freerds() {
+   file { [  "/opt", "/opt/FreeRDS", "/opt/FreeRDS/share", "/opt/FreeRDS/sbin",
+             "/opt/FreeRDS/etc", "/opt/FreeRDS/etc/freerds" ]:
+    ensure => "directory"
+  }
+  file { "/opt/FreeRDS/share/start_script_lib.sh":
+    content => template("epflsti_vdi_students/start_script_lib.sh.erb"),
+  }
+
+  file { "/opt/FreeRDS/sbin/start_greeter":
+    content => template("epflsti_vdi_students/start_greeter.erb"),
+  }
+
+  file { "/opt/FreeRDS/sbin/start_xession":
+    content => template("epflsti_vdi_students/start_xsession.erb"),
+  }
+
+  file { "/opt/FreeRDS/etc/freerds/config.ini":
+    content => template("epflsti_vdi_students/config.ini.erb"),
+  }
+
+  # TODO: this hasn't really been tested and in fact, is quite unlikely
+  # to work. E.g. the script install /usr/local/bin/cmake which links
+  # to a libcurl that doesn't SSL.
+  exec { "wget -O- https://raw.githubusercontent.com/epfl-sti/FreeRDS/master/build-freerds.sh | bash":
+    path => $::path,
+    creates => "/opt/FreeRDS/bin/freerds-manager"
+  }
+}
