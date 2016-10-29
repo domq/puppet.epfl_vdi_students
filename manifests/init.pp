b7afc7f35c7a52e29ea6ef3a4cf4f47e0b01ee20
Dominique Quatravaux <dominique.quatravaux@epfl.ch>
2016-10-29 18:29:02 +0200

    Ensure that we won't shutdown a physical machine

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,7 +1,12 @@
 # Class: epflsti_vdi_students
 #
 # Turn your VM into a master template for a students course.
-
+#
+# === Parameters:
+#
+# $finalize::  Set to true to cause Puppet to purge all staging state
+#              (log files etc) and then shutdown the machine, in preparation
+#              for creating a new snapshot to use as a deployment master.
 class epflsti_vdi_students(
   $finalize = false
 ) {
--- manifests/private/finalize.pp
+++ manifests/private/finalize.pp
@@ -4,6 +4,9 @@
 #
 # This is to be run prior to creating a new snapshot in the template VM.
 class epflsti_vdi_students::private::finalize() {
+  if (! $::is_virtual) {
+    fail("finalize can only be invoked on a virtual machine!")
+  }
   file { ["/opt/FreeRDS/bin/freerds-server.log",
           "/opt/FreeRDS/bin/freerds-manager.log",
           "/var/log/auth.log",
