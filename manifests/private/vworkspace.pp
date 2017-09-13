37a721192a2871494eca9f00b8d751eb9fa2bbfa
Dominique Quatravaux <dominique.quatravaux@epfl.ch>
2017-09-13 18:58:00 +0200

    Only templatify start_* scripts when installing from source

--- manifests/private/vworkspace.pp
+++ manifests/private/vworkspace.pp
@@ -7,37 +7,41 @@
 # * qdcsvc is packaged as a binary-only .deb out of the VPSI-provided master images
 #
 class epflsti_vdi_students::private::vworkspace() {
-   file { [  "/opt", "/opt/FreeRDS", "/opt/FreeRDS/share", "/opt/FreeRDS/sbin",
-             "/opt/FreeRDS/etc", "/opt/FreeRDS/etc/freerds" ]:
-    ensure => "directory"
-  }
-  file { "/opt/FreeRDS/share/start_script_lib.sh":
-    content => template("epflsti_vdi_students/start_script_lib.sh.erb"),
-  }
+  case $::freerds_flavor {
+    "none": {
+      file { [  "/opt", "/opt/FreeRDS", "/opt/FreeRDS/share", "/opt/FreeRDS/sbin",
+                "/opt/FreeRDS/etc", "/opt/FreeRDS/etc/freerds" ]:
+                  ensure => "directory"
+      }
+      file { "/opt/FreeRDS/share/start_script_lib.sh":
+        content => template("epflsti_vdi_students/start_script_lib.sh.erb"),
+      }
 
-  file { "/opt/FreeRDS/sbin/start_greeter":
-    content => template("epflsti_vdi_students/start_greeter.erb"),
-    mode => "0755"
-  }
+      file { "/opt/FreeRDS/sbin/start_greeter":
+        content => template("epflsti_vdi_students/start_greeter.erb"),
+        mode => "0755"
+      }
 
-  file { "/opt/FreeRDS/sbin/start_xsession":
-    content => template("epflsti_vdi_students/start_xsession.erb"),
-    mode => "0755"
-  }
+      file { "/opt/FreeRDS/sbin/start_xsession":
+        content => template("epflsti_vdi_students/start_xsession.erb"),
+        mode => "0755"
+      }
 
-  file { "/opt/FreeRDS/etc/freerds/config.ini":
-    content => template("epflsti_vdi_students/config.ini.erb"),
-  }
+      file { "/opt/FreeRDS/etc/freerds/config.ini":
+        content => template("epflsti_vdi_students/config.ini.erb"),
+      }
 
-  package { ["docbook-xsl", "libwayland-dev"]:
-    ensure => "installed"
-  } ->
-  exec { "Compile FreeRDS from source":
-    command => "wget -O- https://raw.githubusercontent.com/epfl-sti/FreeRDS/master/build-freerds.sh | bash",
-    path => $::path,
-    timeout     => 1800,
-    creates => "/opt/FreeRDS/bin/freerds-manager"
-  }
+      package { ["docbook-xsl", "libwayland-dev"]:
+        ensure => "installed"
+      } ->
+      exec { "Compile FreeRDS from source":
+        command => "wget -O- https://raw.githubusercontent.com/epfl-sti/FreeRDS/master/build-freerds.sh | bash",
+        path => $::path,
+        timeout     => 1800,
+        creates => "/opt/FreeRDS/bin/freerds-manager"
+      }
+    }
+  }  # No FreeRDS available
 
   ensure_resource('class', 'systemd')
   if ($::systemd_available == "true") {
